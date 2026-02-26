29f0c1abea7eeb60edd851c05e8aab2d47f60701
unknown <friddaperez456@gmail.com>
2026-02-25 19:11:34 -0600

    fridda2

--- fluxpay/src/App.jsx
+++ fluxpay/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
-import Dashboard from "./Dashboard";
+import Dashboard from "./Cliente/Dashboard";
 
 function App() {
   return (
