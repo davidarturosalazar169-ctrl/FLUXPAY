ecc14bd30e195deaed91ea39bd98ffe3b02c8cf6
DAVID <david.arturo.salazar.169@gmail.com>
2026-02-25 18:18:10 -0600

    Dashboard y navbar

--- fluxpay/src/App.jsx
+++ fluxpay/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
+import Dashboard from "./Cliente/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+
     </Routes>
   );
 }
